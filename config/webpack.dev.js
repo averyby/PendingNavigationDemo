0c33be8e66a186580c2f45618e09df959d0d0970
Avery By <averyby@yahoo.com>
2019-02-23 23:10:01 +0800

    Wire up webpack-dev-server。 此时在项目根目录运行 ./node_modules/.bin/webpack-dev-server --config=config/webpack.dev.js，便可以对项目成功编译了，控制台显示项目的访问路径是 localhost:8080。webpack-dev-server 不会真实地写文件，所以 webpack-dev-server 并不会向 dist 目录输入任何东西，而是当访问相关路径时，webpack-dev-server 从内存里吐出响应内容（即编译后的静态资源）。至于资源的访问路径是由 contentBase 和 publicPath 决定的。它们就类似于 express 中的 express.static 指定的参数路径，以及为访问静态资源而指定的 express route 的第一个路径参数。
    dist 目录中实际的物理文件也会影响访问结果。当 webpack-dev-server 的内存中没有相应的响应，而目录中存在的物理文件名正好满足请求地址时，物理文件也会被 webpack-dev-server 读取并作为响应发送。比如本项目中 dist 目录中有一个我们手工创建的 index.html，这样访问 localhost:8080 时，webpack-dev-server 便读取磁盘上的这个 index.html 并返回响应的。

---
+++ config/webpack.dev.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: './src/main.js',
+  },
+  mode: 'development',
+  output: {
+    filename: '[name]-bundle.js', // name 会被 entry 名替换
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: "/"
+  },
+  devServer: {
+    // 注意这里指定 dist 目录时没有使用 .. 进入上级目录，原因是这里的相对路径
+    // 是相对于 webpack-dev-server 的命令运行所处的目录的。由于 webpack-dev-server 是在
+    // 项目根目录运行的，而 dist 即是在项目根目录下面，所以这里无需先进入上级目录。
+    contentBase: "dist"
+  }
+};
