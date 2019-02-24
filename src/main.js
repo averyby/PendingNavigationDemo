e7fe7c515bdf1ef97881af145cdc4b0537c5cf32
Avery By <averyby@yahoo.com>
2019-02-24 15:58:07 +0800

    Use presets and regenerator

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
+require('@babel/runtime/regenerator');
 require('./main.css');
 require('./index.html');
 
-var a = () => {
-  console.log('Hello');
+var a = async (args) => {
+  const { a, b } = args;
+  await console.log('Hello', a, b);
+  console.log("Done");
 };
+
+a({ a: 1, b: 2 });
