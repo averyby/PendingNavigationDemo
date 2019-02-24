2e38f2ed021f80cde9e3815924394350c9d58f36
Avery By <averyby@yahoo.com>
2019-02-24 13:58:02 +0800

    Add loaders to process html files.

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -14,7 +14,8 @@ module.exports = {
     // 注意这里指定 dist 目录时没有使用 .. 进入上级目录，原因是这里的相对路径
     // 是相对于 webpack-dev-server 的命令运行所处的目录的。由于 webpack-dev-server 是在
     // 项目根目录运行的，而 dist 即是在项目根目录下面，所以这里无需先进入上级目录。
-    contentBase: "dist"
+    contentBase: "dist",
+    overlay: true
   },
   module: {
     rules: [
@@ -28,7 +29,24 @@ module.exports = {
             loader: 'css-loader'
           }
         ]
-      }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: "[name].html"
+            }
+          },
+          {
+            loader: 'extract-loader'
+          },
+          {
+            loader: 'html-loader'
+          }
+        ]
+      },
     ]
   }
 };
